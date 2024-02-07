7f1b09661c09a5d9ddecc76e4bbffda04b3ae621
cikoedofebrian <ciko.edo.febrians@gmail.com>
2024-02-07 22:59:49 +0700

    feat: create empty case for list survey

--- lib/presentation/survey/list/screen/survey_list_page.dart
+++ lib/presentation/survey/list/screen/survey_list_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:qc_entry/core/extension/text_extension.dart';
 import 'package:qc_entry/core/injector/injector.dart';
+import 'package:qc_entry/core/theme/app_color.dart';
+import 'package:qc_entry/core/theme/app_text.dart';
 import 'package:qc_entry/presentation/survey/list/component/survey_list_item.dart';
 import 'package:qc_entry/presentation/survey/list/provider/survey_list_provider.dart';
 
@@ -31,17 +34,31 @@ class SurveyListView extends StatelessWidget {
           ? const Center(
               child: CircularProgressIndicator(),
             )
-          : ListView.separated(
-              padding: const EdgeInsets.all(24),
-              itemBuilder: (_, index) => SurveyListItem(
-                    deskripsi: surveyListProvider.surveyList[index].deskripsi,
-                    title: surveyListProvider.surveyList[index].judul,
-                    id: surveyListProvider.surveyList[index].id,
+          : surveyListProvider.surveyList.isEmpty
+              ? Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Text(
+                      "Tidak ada survey yang tersedia untuk sekarang",
+                      textAlign: TextAlign.center,
+                      style: AppTextStyle.heading5
+                          .setSemiBold()
+                          .copyWith(color: AppColor.primaryColor),
+                    ),
                   ),
-              separatorBuilder: (_, __) => const SizedBox(
-                    height: 12,
-                  ),
-              itemCount: surveyListProvider.surveyList.length),
+                )
+              : ListView.separated(
+                  padding: const EdgeInsets.all(24),
+                  itemBuilder: (_, index) => SurveyListItem(
+                        deskripsi:
+                            surveyListProvider.surveyList[index].deskripsi,
+                        title: surveyListProvider.surveyList[index].judul,
+                        id: surveyListProvider.surveyList[index].id,
+                      ),
+                  separatorBuilder: (_, __) => const SizedBox(
+                        height: 12,
+                      ),
+                  itemCount: surveyListProvider.surveyList.length),
     );
   }
 }
